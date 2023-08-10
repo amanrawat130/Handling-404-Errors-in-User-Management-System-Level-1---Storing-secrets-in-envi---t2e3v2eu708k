462fda8e0b16dc2c5ead8eaa7ed4524d73e56644
Aman Rawat <56511200+amanrawat130@users.noreply.github.com>
2023-08-10 14:16:35 +0530

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,26 +2,28 @@ const User = require("../models/User");
 const handleAsyncErrors = require("../utils/handleAsyncErrors");
 const handleNotFoundErrors = require("../utils/handleNotFoundErrors");
 
-// Make all the necessary changes to the controller such that it uses the error handlers to handle 404 errors
-
 // Controller with error handling
 const getAllUsers = handleAsyncErrors(async (req, res) => {
   const users = await User.find();
-  //Implement 404 error handling
-  res.status(200).json(users);
+  // Check if users array is empty
+  if (users.length === 0) {
+    // Implement 404 error handling
+    handleNotFoundErrors(res, "User not found");
+  } else {
+    res.status(200).json(users);
+  }
 });
 
 const getUserByID = handleAsyncErrors(async (req, res) => {
   const { id } = req.params;
   const user = await User.findById(id);
-  //Implement 404 error handling
-  res.status(200).json(user);
+  if (!user) {
+    // Implement 404 error handling
+    handleNotFoundErrors(res, "User not found");
+  } else {
+    res.status(200).json(user);
+  }
 });
-
-// Error Response for both controllers: 
-/*
-{ error: "User not found" } Status Code: 404
-*/
 
 module.exports = {
   getAllUsers,
